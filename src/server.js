823472bcf5008660cb97d84b19a83b856dd3a5b7
Haohan Yang <yanghaohanmail@gmail.com>
2026-04-16 23:03:47 +0200

    Fix shutdown bugs

--- src/server.js
+++ src/server.js
@@ -21,39 +21,37 @@ fastify.listen({ port: args.port, host: args.host }, (err, address) => {
 
   console.log(`Server is running at ${address}`);
 
-  // Clean up connections on shutdown
   const shutdown = async (signal) => {
     if (shuttingDown) {
+      console.log('Shutdown already in progress...');
       return;
     }
 
     shuttingDown = true;
     console.log(`Received ${signal}. Shutting down the server...`);
 
-    // 10 seconds timeout to shutdown
     const timeout = setTimeout(() => {
       console.error('Forcefully shutting down after 10 seconds.');
       process.exit(1);
-    }, 10 * 1000);
+    }, 10 * 1000).unref();
 
+    let exitCode = 0;
     try {
-      await connectionManager.close();
-
-      // Terminate mongosh worker runtimes
-      await fastify.workerRuntimeManager.close();
-
       await fastify.close();
       exportIds.close();
+      await fastify.workerRuntimeManager.close();
+      await connectionManager.close();
       console.log('Server closed successfully.');
     } catch (shutdownError) {
       console.error('Error during server shutdown:', shutdownError);
+      exitCode = 1;
     } finally {
       clearTimeout(timeout);
-      process.exit();
+      process.exit(exitCode);
     }
   };
 
   for (const signal of ['SIGINT', 'SIGTERM']) {
-    process.once(signal, () => shutdown(signal));
+    process.on(signal, () => shutdown(signal));
   }
 });
